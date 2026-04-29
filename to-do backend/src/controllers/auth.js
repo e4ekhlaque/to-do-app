21d4cd742b31d4687e0850b274f3b117d7d0db4e
Mohammad Ekhlaque <ekhlaquemd48@outlook.com>
2026-04-29 14:31:38 +0530

    Updated auth.js

--- to-do backend/src/controllers/auth.js
+++ to-do backend/src/controllers/auth.js
@@ -151,7 +151,9 @@ const forgotPassword = async (req, res) => {
     const link = `${process.env.CLIENT_URL}/reset-password/${token}`;
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
